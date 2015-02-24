4fb5477fd93599cadc1ec71b784d695e0a5163e4
Diogo Machado <diogo@diogomachado.com>
2015-02-23 22:40:40 -0300

    Correcao bug

--- www/app/controllers/semana.js
+++ www/app/controllers/semana.js
@@ -68,127 +68,130 @@
 		// Instancio um obj data atual
 		var dt = new Date();
 
-		if ($rootScope.itensLocal[formatar(dt)].horas.length != 0)
-		{
-			// Capturo o número do dia (vai de 0..6) começando 0 por domingo
-			var dayNumber = dt.getDay();
+		if (formatar(dt) in $rootScope.itensLocal){
 
-			// Capturo o dia mesmo (1..31)
-			var day = dt.getDate();
-
-			// Agora acho o primeiro dia da semana
-			dt.setDate(day - (dayNumber-1));
-
-			n = dayNumber;
-
-			// Agora eu vou percorrer a semana
-			while(n <= 7)
+			if ($rootScope.itensLocal[formatar(dt)].horas.length != 0)
 			{
-				var day = dt.getDate(); // Atualiza day
+				// Capturo o número do dia (vai de 0..6) começando 0 por domingo
+				var dayNumber = dt.getDay();
 
-				console.log(formatar(dt));
-				// Verifica se está nos objetos salvos
-				if (formatar(dt) in $rootScope.itensLocal){
+				// Capturo o dia mesmo (1..31)
+				var day = dt.getDate();
 
-					var saldo = parseInt($rootScope.itensLocal[formatar(dt)].saldo);
-					var total = parseInt($rootScope.itensLocal[formatar(dt)].total);
+				// Agora acho o primeiro dia da semana
+				dt.setDate(day - (dayNumber-1));
 
-					totalExecutado += total;
+				n = dayNumber;
 
-					// Divido para achar as horas
-					// ---------------------------------
-					horas = parseInt(saldo/60);
-					minutos = saldo%60;
+				// Agora eu vou percorrer a semana
+				while(n <= 7)
+				{
+					var day = dt.getDate(); // Atualiza day
 
-					// Calcula o total
-					totalHora = totalHora + horas;
-					totalMinutos = totalMinutos + minutos;
+					console.log(formatar(dt));
+					// Verifica se está nos objetos salvos
+					if (formatar(dt) in $rootScope.itensLocal){
 
-					// Muda sinal caso seja positivo
-					if (horas < 0){
-						horas = horas * -1;
+						var saldo = parseInt($rootScope.itensLocal[formatar(dt)].saldo);
+						var total = parseInt($rootScope.itensLocal[formatar(dt)].total);
+
+						totalExecutado += total;
+
+						// Divido para achar as horas
+						// ---------------------------------
+						horas = parseInt(saldo/60);
+						minutos = saldo%60;
+
+						// Calcula o total
+						totalHora = totalHora + horas;
+						totalMinutos = totalMinutos + minutos;
+
+						// Muda sinal caso seja positivo
+						if (horas < 0){
+							horas = horas * -1;
+						}
+
+						if (minutos < 0){
+							minutos = minutos * -1;
+						}
+
+						if (minutos > 60){	
+							restoMinutos = minutos % 60;
+							minutos = minutos + restoMinutos;
+						}
+
+						if (horas < 9){
+							horas = "0"+horas;
+						}
+
+						if (minutos < 9){
+							minutos = "0"+minutos;
+						}
+						// ---------------------------------
+
+						diaUrl = formatar(dt);
+						var re = new RegExp('/', 'g');
+						var diaUrl = diaUrl.replace(re, '-');
+
+						// Crio objeto com as informações
+						objsemana = { 	'dia'            : formatar(dt).substr(0,5),
+						                'diaUrl'         : diaUrl,
+									 	'diaNumero'      : dias_semana[dt.getDay()],
+										'totalTrabalhado': $rootScope.itensLocal[formatar(dt)].total,
+										'saldo'          : saldo,
+										'saldoFmt'       : horas + ":" + minutos };
+
+						// Adiciono no array
+						$scope.objsemana.push(objsemana);
+
 					}
 
-					if (minutos < 0){
-						minutos = minutos * -1;
-					}
-
-					if (minutos > 60){	
-						restoMinutos = minutos % 60;
-						minutos = minutos + restoMinutos;
-					}
-
-					if (horas < 9){
-						horas = "0"+horas;
-					}
-
-					if (minutos < 9){
-						minutos = "0"+minutos;
-					}
-					// ---------------------------------
-
-					diaUrl = formatar(dt);
-					var re = new RegExp('/', 'g');
-					var diaUrl = diaUrl.replace(re, '-');
-
-					// Crio objeto com as informações
-					objsemana = { 	'dia'            : formatar(dt).substr(0,5),
-					                'diaUrl'         : diaUrl,
-								 	'diaNumero'      : dias_semana[dt.getDay()],
-									'totalTrabalhado': $rootScope.itensLocal[formatar(dt)].total,
-									'saldo'          : saldo,
-									'saldoFmt'       : horas + ":" + minutos };
-
-					// Adiciono no array
-					$scope.objsemana.push(objsemana);
-
+					dt.setDate(day + 1); // Seta próxima data
+					n++; // Incrementa
+					horas = 0;
+					minutos = 0;
 				}
 
-				dt.setDate(day + 1); // Seta próxima data
-				n++; // Incrementa
-				horas = 0;
-				minutos = 0;
+				// ----------------------------------------------------------------
+				
+				// Horas que ficaram nos minutos totais
+				if (totalMinutos > 60){	
+					restoTotalHoras = parseInt(totalMinutos/60);
+					totalMinutos = totalMinutos % 60;
+				}
+
+				// Soma o total com o resto das horas
+				totalHora = totalHora + restoTotalHoras;
+
+				// Saldo total em int
+				$scope.saldoTotal = (totalHora * 60) + totalMinutos;
+
+				// Muda sinal caso seja positivo
+				if (totalHora < 0){
+					totalHora = totalHora * -1;
+				}
+
+				if (totalMinutos < 0){
+					totalMinutos = totalMinutos * -1;
+				}
+
+				if (totalMinutos >= 60)
+				{
+					totalHora = totalMinutos/60;
+					totalMinutos = totalMinutos % 60;
+				}
+
+				if (totalHora < 9){
+					totalHora = "0"+totalHora;
+				}
+
+				if (totalMinutos < 9){
+					totalMinutos = "0"+totalMinutos;
+				}
+
+				$scope.totalExecutado = formatarHora(totalExecutado);
+				$scope.saldoTotalFmt = totalHora + ":" + totalMinutos;
 			}
-
-			// ----------------------------------------------------------------
-			
-			// Horas que ficaram nos minutos totais
-			if (totalMinutos > 60){	
-				restoTotalHoras = parseInt(totalMinutos/60);
-				totalMinutos = totalMinutos % 60;
-			}
-
-			// Soma o total com o resto das horas
-			totalHora = totalHora + restoTotalHoras;
-
-			// Saldo total em int
-			$scope.saldoTotal = (totalHora * 60) + totalMinutos;
-
-			// Muda sinal caso seja positivo
-			if (totalHora < 0){
-				totalHora = totalHora * -1;
-			}
-
-			if (totalMinutos < 0){
-				totalMinutos = totalMinutos * -1;
-			}
-
-			if (totalMinutos >= 60)
-			{
-				totalHora = totalMinutos/60;
-				totalMinutos = totalMinutos % 60;
-			}
-
-			if (totalHora < 9){
-				totalHora = "0"+totalHora;
-			}
-
-			if (totalMinutos < 9){
-				totalMinutos = "0"+totalMinutos;
-			}
-
-			$scope.totalExecutado = formatarHora(totalExecutado);
-			$scope.saldoTotalFmt = totalHora + ":" + totalMinutos;
 		}
 
 		this.abrirDia = function(dia){
